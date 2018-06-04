b34b3e4258e468301c7bdf9077843cab98e86bcd
vagrant <vagrant@railsbox>
2018-06-04 14:28:35 +0000

    asocjacja do merchanta

--- db/migrate/20180521174734_create_products.rb
+++ db/migrate/20180521174734_create_products.rb
@@ -1,6 +1,7 @@
 class CreateProducts < ActiveRecord::Migration[5.1]
   def change
     create_table :products do |t|
+      t.belongs_to :merchant, foreign_key: 'merchant_id'
       t.string :name
       t.text :description
       t.integer :suggested_price
