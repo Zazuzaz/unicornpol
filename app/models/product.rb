ce8e6a1e245aacb0c0488c96906ca0372aa73a2e
vagrant <vagrant@railsbox>
2018-06-04 14:40:06 +0000

    poprawka na validacji

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ApplicationRecord
   validates :name, presence: true, length: { maximum: 50}
   validates :description, presence: true, length: { maximum: 500}
-  belongs_to :merchant
+  belongs_to :merchant, foreign_key: 'merchant_id'
 end
