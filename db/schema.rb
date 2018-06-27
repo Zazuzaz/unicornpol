151451f76aff06893293ad02276604a7bd26f9ac
vagrant <vagrant@railsbox>
2018-06-27 13:42:34 +0000

    delete table and model Product

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180607183253) do
+ActiveRecord::Schema.define(version: 20180627133355) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,13 +29,6 @@ ActiveRecord::Schema.define(version: 20180607183253) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "merchants", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "offers", force: :cascade do |t|
     t.text "description", null: false
     t.string "title", null: false
@@ -47,16 +40,6 @@ ActiveRecord::Schema.define(version: 20180607183253) do
     t.datetime "updated_at", null: false
     t.index ["customer_id"], name: "index_offers_on_customer_id"
     t.index ["product_id"], name: "index_offers_on_product_id"
-  end
-
-  create_table "products", force: :cascade do |t|
-    t.bigint "merchantprofile_id"
-    t.string "name", null: false
-    t.text "description", null: false
-    t.integer "suggested_price"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["merchantprofile_id"], name: "index_products_on_merchantprofile_id"
   end
 
 end
--- test/models/product_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class ProductTest < ActiveSupport::TestCase
-  test "nie utworzy produktu bez nazwy i opisu" do
-    product = Product.new
-   assert_not product.save, "utworzył produkt bez nazwy/opisu"
-end
-
-end
