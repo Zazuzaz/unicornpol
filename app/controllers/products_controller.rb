32bc1c45e24341ae0e77a96edede5807d405f73d
vagrant <vagrant@railsbox>
2018-06-28 09:37:40 +0000

    Aktualizacja kontrolerow, widokow i akcji do produktu

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -18,9 +18,16 @@ class ProductsController < ApplicationController
     end
 
     def update
-      product_params = params["product"].permit("name", "description")
-      product = Product.find(params["id"])
+      product_params = params["product"].permit("name", "description", "suggested_price")
+      product = Product.find(params[:id])
       product.update(product_params)
       redirect_to(product_path(product))
     end
+
+    def destroy
+      @product = Product.find(params[:id])
+      @product.destroy
+      redirect_to(products_path(@product))
+    end
+
   end
