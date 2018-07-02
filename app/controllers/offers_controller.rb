fccdc0e3a25a9180d8b4df7bf95541d29d7fba00
Bl4syo <jak.stawicki@gmail.com>
2018-07-02 20:40:35 +0200

    rozwijamy offer controller

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,6 +1,21 @@
 class OffersController < ApplicationController
 
   def new
+    Product.find(params["product_id"])
+    @offer = Offer.new({product_id: params["product_id"]})
+  end
+  def create
+    #puts params
+    Offer.create!(offer_params.merge({customer: Customer.first}))
   end
 
+  private
+    def offer_params
+      params.require(:offer).permit(:title, :description, :suggested_price, :status, :product_id)
+    end
 end
+
+#TODOS:
+
+#obsłużyć błedy w create
+#zwalidować, nie dać wejść na offer new jeśli product nie istnieje
