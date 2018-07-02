402bdf0242cd063958269ab103efd6df7f40e549
Bl4syo <jak.stawicki@gmail.com>
2018-07-02 18:59:59 +0200

    Started offer controller

--- db/schema.rb
+++ db/schema.rb
@@ -29,6 +29,13 @@ ActiveRecord::Schema.define(version: 20180627134946) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "merchants", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "offers", force: :cascade do |t|
     t.text "description", null: false
     t.string "title", null: false
@@ -43,8 +50,8 @@ ActiveRecord::Schema.define(version: 20180627134946) do
   end
 
   create_table "products", force: :cascade do |t|
-    t.string "name"
-    t.text "description"
+    t.string "name", null: false
+    t.text "description", null: false
     t.integer "suggested_price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
