bb641d235b16f319b75cefd9a846ff953703d303
vagrant <vagrant@railsbox>
2018-07-12 10:38:48 +0000

    pull z mastera na porzadki i zmiany w index.html

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   resources :merchantprofile
   resources :products
+  resources :offers
+  
   root 'welcome#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
